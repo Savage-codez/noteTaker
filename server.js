cb36a3192398953d2780c670177c4f46aa210601
Herculano Ramos <herc@Herculanos-MacBook-Pro.local>
2018-11-08 21:39:40 -0500

    yay

--- server.js
+++ server.js
@@ -3,43 +3,28 @@ var path = require("path");
 var connection = require("./db/connection");
 
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "search.html"));
+  res.sendFile(path.join(__dirname, "public/home.html"));
 });
 
 app.get("/addnote", function(req, res) {
-  res.sendFile(path.join(__dirname, "addnote.html"));
+  res.sendFile(path.join(__dirname, "public/addnote.html"));
 });
 
-app.get("/home", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
 
 app.get("/notes", function(req, res) {
   connection.query("SELECT * FROM notes", function(err, dbNotes) {
     res.json(dbNotes);
+    console.log("connect")
   });
 });
 
-// app.get("/api/n/:character", function(req, res) {
-
-//   connection.query("SELECT * FROM notes WHERE name = ? LIMIT 1", [req.params.notes], function(err, dbNotes) {
-//     if (err) throw err;
-
-//     if (dbNotes[0]) {
-//       res.json(dbNotes[0]);
-//     } else {
-//       res.json(null);
-//     }
-//   });
-// });
-
-app.post("/notes", function(req, res) {
+app.post("/addnote", function(req, res) {
   console.log("req.body:", req.body);
 
   connection.query("INSERT INTO notes SET ?", req.body, function(err, result) {
